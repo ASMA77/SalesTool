c869528f4b9f91ad3366435ca89a7cbf6b707165
Compaq <Compaq@192.168.1.5>
2015-12-06 13:50:17 +0000

    Add a simple output change.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,20 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+
+	public static void displayGreeting(){
+		System.out.println("Hello Happy sales people!");
+		System.out.println("This app shows sales data");
+		
+		System.out.println("Test 1");
+	}
+	
+}
